049a37e21097d7fe357f437d05bb73f8fa0c9261
valmir-neto <valmir.neto@icen.ufpa.br>
2026-06-27 15:21:08 -0300

    Update 02schema.sql

--- 02.SCHEMA/02schema.sql
+++ 02.SCHEMA/02schema.sql
@@ -85,3 +85,63 @@ CREATE TABLE pedido (
   data_criacao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   data_atualizacao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE produto (
+    id_produto UUID PRIMARY KEY DEFAULT gen_random_uuid(),      --Entidade Principal
+    id_vendedor UUID NOT NULL REFERENCES vendedor(id_vendedor),  --Está ligado a um vendendor
+    id_categoria INTEGER NOT NULL REFERENCES categoria(id_categoria), --Está ligado a uma categoria 
+    nome VARCHAR(150) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    CONSTRAINT chk_preco_produto
+        CHECK ( preco > 0),                  --Evita Preço negativo 
+    estoque INTEGER NOT NULL DEFAULT 0,     --Cada produto possui apenas um estoque
+    CONSTRAINT chk_estoque_produto
+        CHECK (estoque >= 0),               
+    peso DECIMAL(10,3),
+    CONSTRAINT chk_peso_produto
+        CHECK (peso IS NULL OR peso > 0),                   --O peso pode não ser informado, mais se for evita Peso negativo  
+    status_produto VARCHAR(20) NOT NULL DEFAULT 'ATIVO',    --Ativo é o padrão
+    CONSTRAINT chk_status_produto
+        CHECK (status_produto IN ('ATIVO', 'INATIVO', 'ESGOTADO')),
+    data_cadastro TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP --sera preenchido automaticamente a data cadastro
+);
+
+CREATE TABLE pagamento (
+    id_pagamento UUID PRIMARY KEY DEFAULT gen_random_uuid(),    --Entidade importante! (usa o UUID)
+    id_pedido UUID NOT NULL REFERENCES pedido(id_pedido),       --Todo pagamento esta ligado a 1 pedido
+    metodo_pagamento VARCHAR(20) NOT NULL,                        
+    CONSTRAINT chk_metodo_pagamento
+        CHECK (metodo_pagamento IN ('PIX', 'CARTAO_CREDITO', 'CARTAO_DEBITO', 'BOLETO', 'CARTEIRA')),
+    status_pagamento VARCHAR(20) NOT NULL DEFAULT 'PENDENTE',   --Define pendente como Padrão
+    CONSTRAINT chk_status_pagamento
+        CHECK (status_pagamento IN ('PENDENTE', 'APROVADO', 'RECUSADO', 'ESTORNADO', 'CANCELADO')),
+    valor DECIMAL(10,2) NOT NULL,                               --Mais preciso do que o Float
+    CONSTRAINT chk_valor_pagamento
+        CHECK (valor > 0),                                      --Evitar número negativos
+    codigo_transacao VARCHAR(100) UNIQUE,
+    data_pagamento TIMESTAMP WITH TIME ZONE,                    -- Pode ser nulo pois pagamento pode esta pendente
+    data_criacao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE endereco (
+    id_endereco SERIAL PRIMARY KEY,     --Não é a entidade principal
+    id_usuario UUID NOT NULL REFERENCES usuario(id), --Sempre pertence a um usuário
+    cep VARCHAR(8) NOT NULL, 
+    
+    CONSTRAINT chk_cep                  --Verifica o cep (se não é um cep inválido)
+        CHECK (cep ~ '^[0-9]{8}$'),
+    estado VARCHAR(2) NOT NULL,         --Ocupa menos armazenamento 
+    cidade VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    logradouro VARCHAR(150) NOT NULL,
+    numero VARCHAR(10) NOT NULL,        --Pode ter letras
+    complemento VARCHAR(100),           --Pode ser opcinal
+    tipo_endereco VARCHAR(20) NOT NULL DEFAULT 'CASA', --Casa é definida como padrão
+    
+    CONSTRAINT chk_tipo_endereco        --Verifica o tipo de endereço
+        CHECK (tipo_endereco IN ('CASA', 'APARTAMENTO', 'TRABALHO', 'OUTRO')),
+
+    principal BOOLEAN NOT NULL DEFAULT FALSE, --Usuário escolhe um endereço como padrão
+    data_cadastro TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
