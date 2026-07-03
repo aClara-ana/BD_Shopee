a64a5651d18aeda73f99611ef767f616541e6f8c
samanthavms <samanthav316@gmail.com>
2026-07-03 15:44:05 -0300

    Create 017_minhas_compras.sql

---
+++ 03.SCHEMA/017_minhas_compras.sql
@@ -0,0 +1,15 @@
+SELECT 
+    v.nome_loja AS "Loja",
+    p.eh_indicado AS "Selo Indicado",
+    prod.nome AS "Produto",
+    vp.nome_variacao AS "Variação",
+    ip.preco_original_snapshot AS "Preço Antigo (Riscado)",
+    ip.preco_unitario AS "Preço Pago",
+    ip.quantidade AS "Qtd",
+    ped.valor_total AS "Total Pago"
+FROM pedido ped
+JOIN item_pedido ip ON ped.id_pedido = ip.id_pedido
+JOIN produto prod ON ip.id_produto = prod.id_produto
+JOIN vendedor v ON prod.id_vendedor = v.id_vendedor
+LEFT JOIN variacao_produto vp ON ip.id_variacao = vp.id_variacao
+WHERE ped.status_pedido = 'ENTREGUE'; -- Filtra apenas a aba "Finalizados"
