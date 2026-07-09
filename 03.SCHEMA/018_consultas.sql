2803dff21a7b54ac8d55cdcee854fe0ab256c707
samanthavms <samanthav316@gmail.com>
2026-07-08 21:27:01 -0300

    Add query for total items sold and revenue by store
    Added a new query to retrieve total sold items and gross revenue per store.

--- 03.SCHEMA/018_consultas.sql
+++ 03.SCHEMA/018_consultas.sql
@@ -13,3 +13,15 @@ JOIN produto prod ON ip.id_produto = prod.id_produto
 JOIN vendedor v ON prod.id_vendedor = v.id_vendedor
 LEFT JOIN variacao_produto vp ON ip.id_variacao = vp.id_variacao
 WHERE ped.status_pedido = 'ENTREGUE'; --filtra apenas os finalizados 
+
+
+SELECT 
+    v.nome_loja AS "Nome da Loja",
+    COUNT(ip.id_item_pedido) AS "Total de Itens Vendidos",
+    SUM(ip.subtotal) AS "Faturamento Bruto (R$)"
+FROM vendedor v
+JOIN item_pedido ip ON v.id_vendedor = ip.id_vendedor
+JOIN pedido ped ON ip.id_pedido = ped.id_pedido
+WHERE ped.status_pagamento = 'APROVADO'
+GROUP BY v.nome_loja
+ORDER BY "Faturamento Bruto (R$)" DESC;
