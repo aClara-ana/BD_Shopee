dc14dea11d778f922adff77c6e76eb079bff9f78
valmir-neto <valmir.neto@icen.ufpa.br>
2026-07-09 09:18:41 -0300

    Create 019_add_user.sql

---
+++ 03.SCHEMA/019_add_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO pedido (id_pedido, id_usuario, id_cupom, id_endereco_entrega, valor_produtos, valor_frete, valor_cupom, valor_total, metodo_pagamento, status_pedido, status_pagamento, data_pedido) VALUES
+('12121212-0000-0000-0000-000000000004', '11111111-0000-0000-0000-000000000007', NULL, 6, 63.00, 12.00, 0.00, 75.00, 'PIX', 'CANCELADO', 'NEGADO', '2026-06-10 10:14:15');
